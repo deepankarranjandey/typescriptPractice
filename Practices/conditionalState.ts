c86e9db3f93c84b65d1f21afd02200085334c07a
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-17 13:09:00 +0530

    conditional statememt added

---
+++ Practices/conditionalState.ts
@@ -0,0 +1,39 @@
+
+/**
+ * 
+ * if 
+ * if else
+ * 
+ * 
+ */
+
+
+//if
+
+let myAgeN=20;
+
+if(myAgeN>=18)
+{
+
+    console.log("You are adult")
+}
+
+
+//else if
+
+let num3 =0;
+
+if(num3>0)
+{
+    console.log("This is a positive number")
+}
+
+else if(num3<0)
+{
+    console.log("This is a negative number")   
+}
+
+else{
+    console.log("This is 0 value")
+}
+
