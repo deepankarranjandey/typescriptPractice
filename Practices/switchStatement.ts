c2c2623e3e678eca11ea7137e5572158e7e3cea1
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-17 13:25:13 +0530

    added switches

---
+++ Practices/switchStatement.ts
@@ -0,0 +1,31 @@
+
+
+// switch statements
+
+
+let day=3;
+let dayName="Wednesday";
+
+switch(day)
+{
+    case 1:
+        dayName="Monday";
+        break;
+
+        case 2:
+            dayName="Tuesday";
+            break;
+            
+            case 3:
+                dayName="Wednesday"
+                break;
+                case 4:
+                    dayName="Thursday"
+                    break;
+
+        default:
+            dayName="invalid input"
+
+    
+}
+
