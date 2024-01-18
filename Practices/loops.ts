62f7d0766299e192c3730936fc6fbf5141277f20
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-18 11:26:47 +0530

    loop statement added

--- Practices/loops.ts
+++ Practices/loops.ts
@@ -1,23 +1,57 @@
 
-// for loop statements
+// // for loop statements
 
 
-let n=5
+// let n=5
 
-for(let i=0; i<n; i++)
-{
-    for(let j=0; j<n; j++)
-    {
-        if(j<i)
-        {
-            console.log("*");
-        }
-        else
-        {
-            console.log(" ");
-        }
-        
-    }
-    
-    console.log()  
-}
+// for(let i=0; i<n; i++)
+// {
+//     console.log(i)
+// }
+
+
+// //while loop
+
+// let m=5;
+
+// while(n<=10)
+// {
+//     console.log(m);
+//     n++;
+
+// }
+
+// // do while loop
+
+// let p=10;
+
+// do{
+//     console.log(p);
+// }
+// while(p<14)
+// {
+//     console.log(p);
+//     p++;
+// }
+
+
+// // loop control system
+// // break
+// // continue
+
+
+// let num10=10;
+
+// for(let i=0;i<num10;i++)
+// {
+//     if(i==5)
+//     {
+//         continue;
+//     }
+//     else if (i==7)
+//     {
+//         break;
+//     }
+//     console.log(i);
+// }
+
