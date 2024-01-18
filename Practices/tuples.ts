15b6f0e289bfe5f60d458f624b6965620c64bf3d
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-18 12:10:07 +0530

    tuples added

---
+++ Practices/tuples.ts
@@ -0,0 +1,13 @@
+let persons: [string, number] = ['Tom', 20];
+
+let username: string = persons[0];
+let userAge: number=persons[1];
+
+console.log(username);
+console.log(userAge);
+
+persons[0]='Deepankar';
+persons[1]=30;
+
+console.log(persons);
+
