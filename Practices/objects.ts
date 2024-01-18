4279159c5fd52817e97b6ca7448e22654fb90c87
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-18 12:19:22 +0530

    objects added

---
+++ Practices/objects.ts
@@ -0,0 +1,18 @@
+
+const personNew= {
+    name: 'Jack',
+    age: 26,
+    isStudent: false
+}
+
+ console.log(personNew);
+
+
+ const personNew2: { name: string, age: number, isStudent: boolean} =
+ {
+    name: 'Tom',
+    age: 27,
+    isStudent: true
+ }
+
+ console.log(personNew2);
