9eee85c11a26a8ade64ac29bbef0b32caf87c0f8
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-18 12:48:49 +0530

    more functions added

--- Practices/functions.ts
+++ Practices/functions.ts
@@ -13,3 +13,27 @@ function displayWithParams(price: number, quantity: number)
 
 console.log(displayWithParams(200,3));
 
+
+function greet(name: string, greetMessage?: string)
+{
+    if(greetMessage)
+    {
+        return `Hi ${name}, ${greetMessage}`
+    }
+    else
+    {
+        return `Hi ${name}`
+    }
+}
+
+console.log(greet('Deepankar', 'Welecome to Team'));
+console.log(greet('Deep'));
+
+
+function add (a: number, b: number=0)
+{
+    console.log(a+b);
+}
+
+add(1,2);
+add(1);
