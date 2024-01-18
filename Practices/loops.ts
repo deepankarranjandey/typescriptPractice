54f3dfab714be9780250ea04291c916ca62aa639
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-18 10:36:29 +0530

    added loop statement

---
+++ Practices/loops.ts
@@ -0,0 +1,23 @@
+
+// for loop statements
+
+
+let n=5
+
+for(let i=0; i<n; i++)
+{
+    for(let j=0; j<n; j++)
+    {
+        if(j<i)
+        {
+            console.log("*");
+        }
+        else
+        {
+            console.log(" ");
+        }
+        
+    }
+    
+    console.log()  
+}
